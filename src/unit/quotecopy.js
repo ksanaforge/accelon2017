9abc7ee9b6d37a1c80880702d62a626a2a703e99
yapcheahshen <yapcheahshen@gmail.com>
2017-01-28 10:07:03 +0800

    excerpt copy page number +1

--- src/unit/quotecopy.js
+++ src/unit/quotecopy.js
@@ -1,7 +1,7 @@
 const quoteCopy=function({cor,value,krange}){
 		const r=cor.parseRange(krange);
-		const sp=cor.pageOf(r.start);
-		const ep=cor.pageOf(r.end);
+		const sp=cor.pageOf(r.start)+1;
+		const ep=cor.pageOf(r.end)+1;
 		var pagerange="p."+sp;
 		if (ep!==sp) pagerange="p"+pagerange+'-'+ep;
 		return "「"+value+"」（《"+cor.getGroupName(krange)+"》"+"，"+pagerange+"）";
