5e9e25f6bbfdbe04403b0daca82f4144106a3958
yapcheahshen <yapcheahshen@gmail.com>
2017-03-22 13:13:31 +0800

    for taixu image

--- src/decorators/image.js
+++ src/decorators/image.js
@@ -1,7 +1,12 @@
 const createImage=function({cm,cor,start,end,field,id,tabid,target,actions,fields}){
-	const widget=document.createElement("img");
-	widget.src='data:img/'+field+';base64,'+target;
-	widget.title=cm.doc.getRange(start,end);
+	const container=JSON.stringify(start)==JSON.stringify(end)?"span":"div";
+
+	const widget=document.createElement(container);
+	const img=document.createElement("img");
+	img.src='data:img/'+field+';base64,'+target;
+	img.title=cm.doc.getRange(start,end);
+	widget.appendChild(img);
+
 	if (start==end) {
 		return cm.setBookmark(end,{widget});	
 	} else {
