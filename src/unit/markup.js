d2f8871d41e6f935c8aca0bc83c8d97817cb421b
yapcheahshen <yapcheahshen@gmail.com>
2017-04-26 16:37:15 +0800

    support array format markup

--- src/unit/markup.js
+++ src/unit/markup.js
@@ -2,8 +2,11 @@ const loadMarkup=function(cor,json){
 //	console.time("loadmarkup"); 18K markups take 100ms
 	var out=[];
 	for (var i=0;i<json.length;i++) {
-		var address=json[i].match(/.*?\t/)[0];
-		const kpos=parseInt(address,10);
+		if (typeof json[i]=="string") {
+			json[i]=json[i].split("\t");
+		}
+		var address=json[i][0];
+		const kpos=parseInt(json[i][0],10);
 		if (kpos.toString(10)==address) { //number format
 			address=kpos;
 		}
@@ -12,7 +15,7 @@ const loadMarkup=function(cor,json){
 		const article=cor.articleOf(r.start).at;
 		if (!out[article]) out[article]={pos:[],value:[]};	
 		out[article].pos.push(r.start==r.end?r.start:r.range);
-		out[article].value.push(json[i].substr(address.length));
+		out[article].value.push(json[i][1]);
 	}
 	return out;
 }
